2b791c4f7fe28dc37fafed61c4f971443c12f78b
Antoine Klein <67603819+Nielk74@users.noreply.github.com>
2024-12-30 15:49:56 +0000

    clean return testdb

--- libs/database/db_config.go
+++ libs/database/db_config.go
@@ -29,20 +29,23 @@ func GetConnection() *sql.DB {
 	return db
 }
 
-func (c *Config) testConnection() {
+func (c *Config) testConnection() bool {
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		c.Host, c.Port, c.User, c.Password, c.Database)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
+		return false
 	}
 	defer db.Close()
 
 	if err := db.Ping(); err != nil {
 		log.Fatal(err)
+		return false
 	}
 
 	log.Println("Successfully connected to database!")
+	return true
 }
 
 func RunTestConnection() {
